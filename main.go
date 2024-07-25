8be90cbc229e05528a2abd929fda4d430e73d414
saish24 <saish112@gmail.com>
2024-07-25 16:25:39 +0530

    basic working

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"io"
 	"net"
 	"strconv"
+	"strings"
 )
+
+var hashMap map[string]string
 
 func main() {
 	listener, listenerErr := net.Listen("tcp", ":6379")
@@ -26,46 +29,8 @@ func main() {
 
 		defer clientConnection.Close()
 
-		for {
-			buffer := make([]byte, 1024)
-			_, readErr := clientConnection.Read(buffer)
-			if readErr != nil {
-				if readErr == io.EOF {
-					fmt.Println("Client disconnected", clientConnection.RemoteAddr())
-					break
-				}
-				panic(readErr)
-			}
-
-			// reading input
-			reader := bufio.NewReader(bytes.NewBuffer(buffer))
-			b, err := reader.ReadByte()
-			if err != nil {
-				panic(err)
-			}
-			if b != byte('*') {
-				fmt.Println(fmt.Sprintf("Invalid byte received - %b. Command skipped: %v", b, clientConnection.RemoteAddr()))
-				continue
-			}
-
-			commandCount, err := getInt(reader)
-			if err != nil {
-				fmt.Println(fmt.Sprintf("Invalid byte received - %b. Command skipped: %v", b, clientConnection.RemoteAddr()))
-				continue
-			}
-			reader.ReadByte()
-			reader.ReadByte()
-
-			commands, err := getCommands(reader, commandCount)
-			if err != nil {
-				fmt.Println(fmt.Sprintf("Error in processing command - %v. Command skipped: %v", err, clientConnection.RemoteAddr()))
-				continue
-			}
-
-			fmt.Println(commands)
-
-			clientConnection.Write([]byte("+OK\r\n"))
-		}
+		hashMap = make(map[string]string)
+		handleClientConnection(clientConnection)
 	}
 }
 
@@ -91,8 +56,7 @@ func getCommands(reader *bufio.Reader, commandCount int64) ([]string, error) {
 		reader.ReadByte()
 
 		str := make([]byte, length)
-		_, err = reader.Read(str)
-		if err != nil {
+		if _, err = reader.Read(str); err != nil {
 			return nil, err
 		}
 		commands = append(commands, string(str))
@@ -101,4 +65,66 @@ func getCommands(reader *bufio.Reader, commandCount int64) ([]string, error) {
 	}
 
 	return commands, nil
+}
+
+func handleClientConnection(clientConnection net.Conn) {
+	for {
+		buffer := make([]byte, 1024)
+		_, readErr := clientConnection.Read(buffer)
+		if readErr != nil {
+			if readErr == io.EOF {
+				fmt.Println("Client disconnected", clientConnection.RemoteAddr())
+				break
+			}
+			panic(readErr)
+		}
+
+		// reading input
+		reader := bufio.NewReader(bytes.NewBuffer(buffer))
+		b, err := reader.ReadByte()
+		if err != nil {
+			panic(err)
+		}
+		if b != byte('*') {
+			fmt.Println(fmt.Sprintf("Invalid byte received - %b. Command skipped: %v", b, clientConnection.RemoteAddr()))
+			continue
+		}
+
+		commandCount, err := getInt(reader)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Invalid byte received - %b. Command skipped: %v", b, clientConnection.RemoteAddr()))
+			continue
+		}
+		reader.ReadByte()
+		reader.ReadByte()
+
+		commands, err := getCommands(reader, commandCount)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Error in processing command - %v. Command skipped: %v", err, clientConnection.RemoteAddr()))
+			continue
+		}
+
+		result := processCommands(commands)
+
+		clientConnection.Write([]byte(fmt.Sprintf("+%s\r\n", result)))
+	}
+}
+
+func processCommands(commands []string) string {
+	switch strings.ToLower(commands[0]) {
+	case "ping":
+		return "PONG"
+	case "get":
+		if len(commands) > 1 {
+			key := commands[1]
+			if val, found := hashMap[key]; found {
+				return val
+			}
+		}
+	case "set":
+		if len(commands) > 2 {
+			hashMap[commands[1]] = commands[2]
+		}
+	}
+	return ""
 }
